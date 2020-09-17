9081cca0a6c42059de5c40c78999377d7fce573b
Muhammad Afaque <62635673+afaqshaikh@users.noreply.github.com>
2020-09-17 12:32:26 +0500

    Add firebase signin and signout function

--- Chat App/app.js
+++ Chat App/app.js
@@ -13,7 +13,7 @@ let showChatBox = () => {
 //hideChatBox function
 let hideChatBox = () => {
     document.getElementById('side-1').classList.add('d-none', 'd-md-block')
-    document.getElementById('sude-2').classList.remove('d-none')
+    document.getElementById('side-2').classList.remove('d-none')
 }
 
 //Send Message
@@ -38,9 +38,59 @@ let sendMessage = () => {
         <img src="Images/user.png" alt="user" class="chat-image">
     </div>
 </div>`
-document.getElementById('messages').innerHTML += message
-document.getElementById('textMessage').value = ''
-document.getElementById('textMessage').focus()
+    document.getElementById('messages').innerHTML += message
+    document.getElementById('textMessage').value = ''
+    document.getElementById('textMessage').focus()
 
-document.getElementById('messages').scrollTo(0,document.getElementById('messages').clientHeight)
+    document.getElementById('messages').scrollTo(0, document.getElementById('messages').clientHeight)
 }
+
+//signIn function
+let signIn = () => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+        .then(function (result) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            // ...
+            console.log(result)
+        }).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+            console.log(error)
+        });
+}
+
+//signout function
+let signOut = () => {
+    firebase.auth().signOut()
+        .then(function () {
+            // Sign-out successful.
+        }).catch(function (error) {
+            // An error happened.
+        });
+}
+
+//firebase state changed
+let onFirebaseStateChanged = () => {
+    firebase.auth().onAuthStateChanged(onStateChanged)
+}
+
+let onStateChanged = (user) => {
+    if(user){
+        // alert(firebase.auth().currentUser.email + '\n' + firebase.auth().currentUser.displayName)
+        document.getElementById('imgProfile').src = firebase.auth().currentUser.photoURL
+        document.getElementById('imgProfile').title = firebase.auth().currentUser.displayName
+    }
+}
+
+//call firebae state
+onFirebaseStateChanged()
