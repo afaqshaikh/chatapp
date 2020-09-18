7712634df22a802394ac50dee1ae25630139ea00
Muhammad Afaque <62635673+afaqshaikh@users.noreply.github.com>
2020-09-18 21:27:17 +0500

    Add firebase

--- Chat App/app.js
+++ Chat App/app.js
@@ -1,7 +1,32 @@
-let startChat = (id) => {
-    document.getElementById('chatpanel').removeAttribute('style')
-    document.getElementById('divstart').setAttribute('style', 'display:none;')
-    hideChatBox()
+var currentuserKey = ''
+///
+let startChat = (friendKey , friendName , friendPhoto) => {
+    
+    var friendList = {
+        friendId : friendKey,
+        userId: currentuserKey
+    }
+
+    var database = firebase.database().ref('friend_list')
+    var flag = false
+    database.on('value',(friends) => {
+        friends.forEach( (data) => {
+            var user  = data.val()
+            if((user.friendId === friendList.friendId && user.userId === friendList.userId) || (user.friendId === friendList.userId && user.userId === friendList.friendId)){
+                flag = true
+            }
+        })
+    })
+    firebase.database().ref('friend_list').push(friendList , (error) => {
+        if(error){
+            alert(error)
+        }else{
+            document.getElementById('chatpanel').removeAttribute('style')
+            document.getElementById('divstart').setAttribute('style', 'display:none;')
+            hideChatBox()
+        
+        }
+    })
 }
 
 //showChatBox function
@@ -45,6 +70,51 @@ let sendMessage = () => {
     document.getElementById('messages').scrollTo(0, document.getElementById('messages').clientHeight)
 }
 
+//populateFriendList
+let populateFriendList = () => {
+    document.getElementById('lstFriend').innerHTML = `
+    <div class="text-center">
+        <span class="spinner-border text-primary mt-5" style="width:5rem;height:5rem;"></span>
+    </div>
+    `
+    var database = firebase.database().ref('users')
+     var list = ''
+    database.on('value', (users) => {
+       if(users.hasChildren()){
+           list = `
+           <li class="list-group-item" style="background-color: #f8f8f8;">
+           <input type="text" class="form-control form-rounded" placeholder="Search..."
+               aria-label="Username" aria-describedby="basic-addon1">
+       </li>
+           `
+       }
+       users.forEach( (data) =>{
+        var user = data.val()
+        if(user.email !== firebase.auth().currentUser.email){
+            list += `
+            <li class="list-group-item list-group-item-action" data-dismiss="modal" onclick="startChat('${data.key}','${user.name}','${user.photoURL}')">
+            <div class="row">
+                <div class="col-md-2">
+                    <img src="${user.photoURL}" class="rounded-circle friend-image" alt="user">
+                </div>
+                <div class="col-md-10" style="cursor: pointer;">
+                    <div class="name">${user.name}</div>
+                    <div class="message">Hey there i am using whatsapp...</div>
+                </div>
+            </div>
+        </li>
+            `
+    
+        }
+       
+       })
+    document.getElementById('lstFriend').innerHTML = list
+
+    })
+
+
+}
+
 //signIn function
 let signIn = () => {
     var provider = new firebase.auth.GoogleAuthProvider();
@@ -85,10 +155,59 @@ let onFirebaseStateChanged = () => {
 }
 
 let onStateChanged = (user) => {
-    if(user){
+    if (user) {
         // alert(firebase.auth().currentUser.email + '\n' + firebase.auth().currentUser.displayName)
+
+        var userProfile = { email: '', name: '', photoURL: '', }
+        userProfile.email = firebase.auth().currentUser.email
+        userProfile.name = firebase.auth().currentUser.displayName
+        userProfile.photoURL = firebase.auth().currentUser.photoURL
+        var database = firebase.database().ref('users')
+        var flag = false
+        database.on('value', (users) => {
+            users.forEach((data) => {
+                var user = data.val()
+                if (user.email === userProfile.email){
+                   currentuserKey = data.key
+                    flag = true
+
+                }
+            })
+            if (flag === false) {
+                firebase.database().ref('users').push(userProfile, callback)
+
+            } else {
+                document.getElementById('imgProfile').src = firebase.auth().currentUser.photoURL
+                document.getElementById('imgProfile').title = firebase.auth().currentUser.displayName
+
+                document.getElementById('lnkSignIn').style = 'display:none;'
+                document.getElementById('lnkSignOut').style = ''
+            }
+        document.getElementById('lnkNewChat').classList.remove('disabled')
+
+        })
+
+    } else {
+        document.getElementById('imgProfile').src = 'Images/user.png'
+        document.getElementById('imgProfile').title = ''
+
+        document.getElementById('lnkSignIn').style = ''
+        document.getElementById('lnkSignOut').style = 'display:none;'
+
+        document.getElementById('lnkNewChat').classList.add('disabled')
+    }
+}
+
+let callback = (error) => {
+    if (error) {
+        alert(error)
+    }
+    else {
         document.getElementById('imgProfile').src = firebase.auth().currentUser.photoURL
         document.getElementById('imgProfile').title = firebase.auth().currentUser.displayName
+
+        document.getElementById('lnkSignIn').style = 'display:none;'
+        document.getElementById('lnkSignOut').style = ''
     }
 }
 
